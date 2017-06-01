421de5e5c446fc771fe709c5b39ad3df6283ee75
jiangzuru <zzasss111@gamil.com>
2017-06-01 17:27:42 +0800

    0601 文档与sql更新

--- ThinkPHP/Library/QL/QueryList.class.php
+++ ThinkPHP/Library/QL/QueryList.class.php
@@ -256,6 +256,7 @@ class QueryList
     {
         if(function_exists('curl_init')){
             $ch = curl_init();
+
             curl_setopt($ch, CURLOPT_URL, $url);
             curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
             curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
